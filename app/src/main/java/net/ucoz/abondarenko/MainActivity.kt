7a02b6da8e0c642e5cf595f2948a653e69927eb0
Bondarenko <aleksejbondarenko18@gmail.com>
2022-12-02 14:37:06 +0200

    added topBar

--- app/src/main/java/net/ucoz/abondarenko/MainActivity.kt
+++ app/src/main/java/net/ucoz/abondarenko/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
 import net.ucoz.abondarenko.navigation.NoteNavHost
 import net.ucoz.abondarenko.ui.theme.NotesAppTheme
 
@@ -18,13 +19,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NotesAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    NoteNavHost()
-                }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = "Notes App") },
+                            backgroundColor = Color.Blue,
+                            contentColor = Color.White,
+                            elevation = 8.dp
+                        )
+                    },
+                    content = {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colors.background
+                        ) {
+                            NoteNavHost()
+                        }
+
+                    }
+                )
             }
         }
     }
