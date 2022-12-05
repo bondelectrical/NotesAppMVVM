6382d1a66a2440555c736d0680d8781beeba8bc1
Bondarenko <aleksejbondarenko18@gmail.com>
2022-12-05 13:30:03 +0200

    refactoring

--- app/src/main/java/net/ucoz/abondarenko/MainActivity.kt
+++ app/src/main/java/net/ucoz/abondarenko/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
 import net.ucoz.abondarenko.navigation.NoteNavHost
 import net.ucoz.abondarenko.ui.theme.NotesAppTheme
 
