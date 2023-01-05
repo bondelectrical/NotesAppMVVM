048febbb8c8fef30b554fc0f62b6773ca8229f1e
Bondarenko <aleksejbondarenko18@gmail.com>
2023-01-05 21:58:35 +0200

    added scrollBarIndicator

--- app/src/main/java/net/ucoz/abondarenko/screen/Main.kt
+++ app/src/main/java/net/ucoz/abondarenko/screen/Main.kt
@@ -2,22 +2,36 @@ package net.ucoz.abondarenko.screen
 
 import android.annotation.SuppressLint
 import android.app.Application
+import android.util.Log
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.times
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -26,16 +40,17 @@ import net.ucoz.abondarenko.MainViewModelFactory
 import net.ucoz.abondarenko.model.Note
 import net.ucoz.abondarenko.navigation.Screen
 import net.ucoz.abondarenko.ui.theme.NotesAppTheme
+import net.ucoz.abondarenko.ui.theme.Purple700
+import net.ucoz.abondarenko.utils.*
 import net.ucoz.abondarenko.utils.Constants.Keys.EMPTY
-import net.ucoz.abondarenko.utils.DB_TYPE
-import net.ucoz.abondarenko.utils.TYPE_FIREBASE
-import net.ucoz.abondarenko.utils.TYPE_ROOM
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen(navHostController: NavHostController, viewModel: MainViewModel) {
 
     val notes = viewModel.readAllNotes().observeAsState(listOf()).value
+    val listState = rememberLazyListState()
+
 
     Scaffold(
         floatingActionButton = {
@@ -50,12 +65,25 @@ fun MainScreen(navHostController: NavHostController, viewModel: MainViewModel) {
             }
 
         }) {
-        LazyColumn {
-            items(notes) { note ->
-                NoteItem(note = note, navHostController = navHostController)
+        LazyColumnScrollbar(
+            listState
+        ) {
+            LazyColumn(state = listState) {
+                items(notes) { note ->
+                    NoteItem(note = note, navHostController = navHostController)
 
+                }
             }
         }
+
+
+//        LazyColumn() {
+//            items(notes) { note ->
+//                NoteItem(note = note, navHostController = navHostController)
+//
+//            }
+//        }
+
     }
 
 }
@@ -72,7 +100,7 @@ fun NoteItem(note: Note, navHostController: NavHostController) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp, 8.dp)
+            .padding(start = 8.dp, top = 8.dp, end = 16.dp, bottom = 8.dp)
             .clickable { navHostController.navigate(route = Screen.Note.route + "/${noteId}") },
         elevation = 8.dp
     ) {
@@ -89,6 +117,9 @@ fun NoteItem(note: Note, navHostController: NavHostController) {
         }
     }
 }
+
+
+
 
 @Preview(showBackground = true)
 @Composable
